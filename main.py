530bbd06fb9858063ca89fa2485d29783bc5af17
Vikash <phanivikash2@gmail.com>
2025-04-01 23:02:45 +0530

    Connecting Code with Sqlite 3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import (QApplication,QLabel ,QWidget ,
-        QGridLayout ,QLineEdit , QPushButton , QMainWindow , QTableWidget)
+from PyQt6.QtWidgets import (QApplication, QLabel, QWidget,
+                             QGridLayout, QLineEdit, QPushButton, QMainWindow,
+                             QTableWidget, QTableWidgetItem, QDialog,QVBoxLayout,QComboBox)
 from PyQt6.QtGui import QAction
 import sys
 import sqlite3
@@ -14,6 +15,7 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu("&Help")
 
         add_student_action = QAction("Add Student",self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
 
@@ -23,11 +25,61 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("ID","Name","Course","Mobile"))
+        self.table.verticalHeader().setVisible(False)
+
         self.setCentralWidget(self.table)
 
     def load_data(self):
 
-        pass
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number,row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number,data in enumerate(row_data):
+                print(row_data)
+                self.table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+        connection.close()
+
+    def insert(self):
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        #Set layout
+        self.setWindowTitle("Insert New Student Details")
+        self.setFixedHeight(300)
+        self.setFixedWidth(300)
+
+        layout = QVBoxLayout()
+
+        #Add student name
+        '''student_name_label = QLabel("Name of student")
+        layout.addWidget(student_name_label)'''
+
+        student_name = QLineEdit()
+        student_name.setPlaceholderText("Name of the Student :")
+        layout.addWidget(student_name)
+
+        # Add courses using Combobox
+        course_name = QComboBox()
+        courses = ["Biology",'Maths','Physics','Chemistry','Telugu']
+        course_name.addItems(courses)
+        layout.addWidget(course_name)
+
+        # Add mobile Number Widget
+        mobile_number = QLineEdit()
+        mobile_number.setPlaceholderText("Enter Mobile Number :")
+        layout.addWidget(mobile_number)
+
+
+
+
+        self.setLayout(layout)
 
 
 
@@ -36,4 +88,5 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 main_window = MainWindow()
 main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
